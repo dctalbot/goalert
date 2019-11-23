83703d7754df9d2a86e2461a8f751b1853cb9a0f
dctalbot <dctalbot@umich.edu>
2019-11-22 23:18:30 -0600

    wip: fail silently on null serviceworker bug

--- web/src/app/push-notifications/index.js
+++ web/src/app/push-notifications/index.js
@@ -1,34 +1,41 @@
 import { createNotificationSubscription } from './util'
 
-// Register a Service Worker.
-function register() {
-  navigator.serviceWorker.register('./sw.js')
+async function register() {
+  // clean slate?
+  // navigator.serviceWorker.getRegistrations().then(registrations => {
+  //   for (let registration of registrations) {
+  //     registration.unregister()
+  //   }
+  // })
 
-  createNotificationSubscription().then(subscription => {
+  const registration = await navigator.serviceWorker.register('./sw.js')
+
+  // TODO fix race condition here b/w state of installing vs activated
+
+  if (registration.active) {
+    const subscription = await createNotificationSubscription()
     const payload = JSON.stringify(subscription)
     console.log(payload)
+  } else {
+    console.log('sorry, try again later')
+  }
 
-    // TODO set up data model to store json payload
-    // Send the subscription details to the server using the Fetch API.
-    // fetch("./register", {
-    //   method: "post",
-    //   headers: {
-    //     "Content-type": "application/json"
-    //   },
-    //   body: JSON.stringify({
-    //     subscription: payload
-    //   })
-    // });
-  })
+  // TODO set up data model to store json payload
+  // Send the subscription details to the server using the Fetch API.
+  // fetch("./register", {
+  //   method: "post",
+  //   headers: {
+  //     "Content-type": "application/json"
+  //   },
+  //   body: JSON.stringify({
+  //     subscription: payload
+  //   })
+  // });
 }
 
-// Unregister a Service Worker.
-function unregister() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(registration => {
-      registration.unregister()
-    })
-  }
+async function unregister() {
+  const registration = await navigator.serviceWorker.ready
+  registration.unregister()
 }
 
 export { register, unregister }
